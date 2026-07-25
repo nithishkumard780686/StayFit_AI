ee2b9b7289e0674443dcf7bf100639d9bd7b522b
nithishkumard780686 <nithishkumar2575@gmail.com>
2026-07-25 09:52:10 +0530

    Fix ReferenceError by importing useNavigate in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import LoginSignup from "./Components/Auth/LoginSignup";
 import GeneratorPage from "./Components/Generator/GeneratorPage";
 import DashboardPage from "./Components/Dashboard/DashboardPage";
